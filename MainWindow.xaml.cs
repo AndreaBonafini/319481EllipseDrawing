01910369413484c0c5854a45a40732918cf4f40b
AndreaBonafini <47528585+AndreaBonafini@users.noreply.github.com>
2019-03-22 10:03:52 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿/*Andrea Bonafini
+ *March 22,2019
+ *Ellipse drawing 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _319481EllipseDrawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            for (int i = 9; i > 0; i--)
+            {
+                    InitializeComponent();
+                    Ellipse e = new Ellipse();
+                    e.Width = i * 100 ;
+                    e.Height = i * 100 ;
+                    
+                                        
+                    if (i % 2 == 0 )
+                    {
+                    e.Fill = Brushes.Black;                       
+                    }
+                    else
+                    {
+                    e.Fill = Brushes.Gold;
+                    }
+                    canvas.Children.Add(e);
+                    Canvas.SetTop(e, 480 -(e.Width / 2));
+                    Canvas.SetLeft(e,  480 - (e.Width / 2));
+                }
+        }
+         
+            }
+
+        }
+    }
